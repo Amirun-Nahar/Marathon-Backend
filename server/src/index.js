9ceff95bf8d32b3c88754e17d647826302d703df
Amirun Nahar <naharamina68@gmail.com>
2025-07-01 13:50:06 +0600

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://runlytic-marathon.netlify.app/', 'http://localhost:5173'],
+  origin: ['https://runlytic-marathon.netlify.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
